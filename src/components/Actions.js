118acda932a4cda03b4e3d7233ccc556a1779462
Alex Cyphus <alexjcyphus@gmail.com>
2020-09-06 13:16:56 +0200

    forgot to link the links lol

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -6,7 +6,7 @@ function Actions({toggleOverlay, toggleCard}) {
     <div className="inner-actions m-auto container">
       <div className="row">
         <div className="col-sm-6 m-0 clickable action" onClick={toggleOverlay}><p className="m-0"><Emoji emoji="🗂️" desc="files"/> projects <Emoji emoji="🗂️" desc="files"/></p></div>
-        <div className="col-sm-6 m-0 action" onClick={toggleCard}><p className="m-0"><Emoji emoji="👋" desc="waving"/> contact me <Emoji emoji="👋" desc="waving"/></p></div>
+        <div className="col-sm-6 m-0 action clickable" onClick={toggleCard}><p className="m-0"><Emoji emoji="👋" desc="waving"/> contact me <Emoji emoji="👋" desc="waving"/></p></div>
       </div>
     </div>
   ])
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,11 +5,10 @@ function Card({toggleCard, display}) {
   return ([
     <div className="outer-card" style={{display: display}} onClick={toggleCard}>
       <div className="m-auto row inner-card d-flex container drop-shadow-black">
-      <div id="close-card-row col-12" style={{position: "absolute"}}>
+      <div id="close-card-row col-12" className="clickable tn-2" style={{position: "absolute"}}>
         <p id="close-card" className="col-auto" onClick={toggleCard}>x</p>
       </div>
         <div className="inner-inner-card row d-flex m-auto">
-
           <div className="container m-auto mx-5">
             <div className="row margin-auto">
               <div className="col-auto m-auto" id="my-face-container">
@@ -17,10 +16,10 @@ function Card({toggleCard, display}) {
               </div>
               <div className="col-auto m-auto" id="links">
                 <p id="my-name">alex (john) cyphus</p>
-                <p className="biz-card-link">📍 berlin, germany</p>
-                <p className="biz-card-link pushed-in">🌏 alexcyph.us</p>
-                <p className="biz-card-link pushed-in">📩 alexjcyphus@gmail.com</p>
-                <p className="biz-card-link">💼 linkedin.com/in/alexcyphus</p>
+                <p className="biz-card-link"><Emoji emoji="📍" desc="pin"/> berlin, germany</p>
+                <a className="tn-2" target="_blank" rel="noopener noreferrer" href="http://www.alexcyph.us"><p className="clickable biz-card-link pushed-in"><Emoji emoji="🌏" desc="planet"/> alexcyph.us</p></a>
+                <a className="tn-2" target="_blank" rel="noopener noreferrer" href="mailto:alexjcyphus@gmail.com"><p className="clickable biz-card-link pushed-in"><Emoji emoji="📩" desc="email"/> alexjcyphus@gmail.com</p></a>
+                <a className="tn-2" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alexcyphus/"><p className="clickable biz-card-link"><Emoji emoji="💼" desc="briefcase"/> linkedin.com/in/alexcyphus</p></a>
               </div>
             </div>
           </div>
