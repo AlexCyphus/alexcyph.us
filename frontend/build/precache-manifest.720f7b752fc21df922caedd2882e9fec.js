005319d214164f800324e77178b6fd2fa6974f74
Alex Cyphus <alexjcyphus@gmail.com>
2021-04-29 19:38:06 +0200

    add backend

---
+++ frontend/build/precache-manifest.720f7b752fc21df922caedd2882e9fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d79c706cd68d6d07646d81e8cc805a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c369172d1024a038cc97",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ace29dbe29a016908ed1",
+    "url": "/static/css/main.0653f499.chunk.css"
+  },
+  {
+    "revision": "c369172d1024a038cc97",
+    "url": "/static/js/2.e97aa466.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e97aa466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace29dbe29a016908ed1",
+    "url": "/static/js/main.4e687aeb.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
