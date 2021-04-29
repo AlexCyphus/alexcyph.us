35cb7c2fb1b7046dd32a26c84689e91ce1aeb6b1
Alex Cyphus <alexjcyphus@gmail.com>
2021-04-29 17:44:47 +0200

    add backend

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
   const passwords = "hello world"
-
   // Return them as json
   res.json(passwords);
-  console.log('test')
+  console.log('api/passwords')
 });
 
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
+    console.log(req.url)
     let buildFile = path.join(__dirname+'/client/build/index.html')
     res.sendFile(buildFile);
+    console.log()
 });
 
 const port = process.env.PORT || 5000;
