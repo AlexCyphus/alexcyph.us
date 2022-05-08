22254ba986e0737e78986b1543ad57ee8de6b79a
Alex Cyphus <alexjcyphus@gmail.com>
2022-05-09 00:48:35 +0200

    add monaco

--- frontend/src/components/Sections/TravelSection/Globe/data/countryData.ts
+++ frontend/src/components/Sections/TravelSection/Globe/data/countryData.ts
@@ -1,7 +1,7 @@
 import geoData from './geoData.json'
 
 export const countriesVisited = ['Canada', 'United States of America', 'Belize', 'Colombia', 'Peru', 'United Kingdom', 'Sweden',
-  'Denmark', 'Germany', 'Spain', 'Italy', 'Portugal', 'Hungary', 'Belgium', 'Taiwan', 'France', 'Austria', 'Poland', 'Croatia', 'Czechia']
+  'Denmark', 'Germany', 'Spain', 'Italy', 'Portugal', 'Hungary', 'Belgium', 'Taiwan', 'France', 'Austria', 'Poland', 'Croatia', 'Czechia', 'Monaco']
 
 export const globeCountriesData = geoData
   .features.filter((feature) => !countriesVisited.includes(feature.properties.ADMIN))
