0ff6fc175c0ea1a440b2f4d5b4f542f864742eed
Alex Cyphus <alexjcyphus@gmail.com>
2022-05-09 00:40:39 +0200

    small tweaks

--- frontend/src/components/HeaderAnimation/HeaderAnimation.tsx
+++ frontend/src/components/HeaderAnimation/HeaderAnimation.tsx
@@ -4,14 +4,15 @@ const HeaderAnimation = () => {
   const frame = useCycleHeaderAnimation()
 
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex flex-col gap-y-2 items-center">
       <div className="p-5 max-w-sm h-40 flex items-center justify-center">
         <img
-          alt="alex cyphus"
+          alt="alex title animation"
           src={frame}
           className="w-sm"
         />
       </div>
+      <p className="italic text-sm text-center">Welcome to my corner of the internet</p>
     </div>
   )
 }
--- frontend/src/components/Layout/SectionLayout.tsx
+++ frontend/src/components/Layout/SectionLayout.tsx
@@ -3,7 +3,7 @@ interface ISectionLayout {
 }
 
 const SectionLayout: React.FC<ISectionLayout> = ({ title, children }) => (
-  <div className="flex flex-col gap-y-16 pt-20 items-center z-20">
+  <div className="flex flex-col gap-y-16 pt-16 items-center z-20">
     <p className="text-3xl bg-black text-white py-2 px-16 rounded-lg">{title}</p>
     {children}
   </div>
