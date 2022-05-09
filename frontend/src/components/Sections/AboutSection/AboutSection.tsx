1ac7e6a2dfac50402321c4d22348bbaf834f9410
Alex Cyphus <alexjcyphus@gmail.com>
2022-05-09 12:43:35 +0200

    max-w

--- frontend/src/components/Sections/AboutSection/AboutSection.tsx
+++ frontend/src/components/Sections/AboutSection/AboutSection.tsx
@@ -4,7 +4,7 @@ import AboutLine from './AboutLine'
 
 const AboutSection: React.FC = () => (
   <SectionLayout title="About">
-    <div className="flex flex-col gap-y-3 items-start w-full">
+    <div className="flex flex-col gap-y-3 items-start w-full max-w-6xl">
       {aboutData.map((row) => (
         <AboutLine key={JSON.stringify(row)}>
           {row}
